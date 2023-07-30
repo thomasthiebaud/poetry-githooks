90df5fdcfea22995ce9057999356bcdaa37a728b
Thomas Thiebaud <thiebaud.tom@gmail.com>
2023-07-30 20:38:37 +0200

    Update dependencies and release version 2.0.1

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -2,39 +2,41 @@ import sys
 from unittest.mock import patch
 import toml
 
-from mamba import context, describe, it
 from poetry_githooks import helpers
 
-with describe("Helpers"):
-    with context("#execute_script"):
-        with it("should execute a script without arguments"):
-            with patch("subprocess.call") as call_mock, patch("sys.exit") as exit_mock:
-                helpers.execute_script("ls")
-                call_mock.assert_called_with("ls", shell=True)
-        with it("should execute a script with static arguments"):
-            with patch("subprocess.call") as call_mock, patch("sys.exit") as exit_mock:
-                helpers.execute_script("ls -l")
-                call_mock.assert_called_with("ls -l", shell=True)
-        with it(
-            "should execute a script with dynamic arguments and escape them if necessary"
-        ):
-            with patch("subprocess.call") as call_mock, patch("sys.exit") as exit_mock:
-                helpers.execute_script("ls", ["-l", "somefile; rm -rf ~"])
-                call_mock.assert_called_with("ls -l 'somefile; rm -rf ~'", shell=True)
-    with context("#read_config"):
-        with it("should exit if the config is not present"):
-            with patch("os.path.isfile", return_value=False) as isfile_mock, patch(
-                "sys.exit"
-            ) as exit_mock, patch("toml.load") as toml_mock:
-                helpers.read_config()
-                args, kwargs = exit_mock.call_args
-                assert args[0] == 1
-        with it("should exit if config is present but malformed"):
-            with patch("os.path.isfile", return_value=True) as isfile_mock, patch(
-                "sys.exit"
-            ) as exit_mock, patch(
-                "toml.load", side_effect=toml.TomlDecodeError("Error", "", 0)
-            ) as toml_mock:
-                helpers.read_config()
-                args, kwargs = exit_mock.call_args
-                assert args[0] == 1
+from unittest import TestCase
+
+
+class TestHelpers(TestCase):
+    def test_execute_script_without_argument(self):
+        with patch("subprocess.call") as call_mock, patch("sys.exit") as exit_mock:
+            helpers.execute_script("ls")
+            call_mock.assert_called_with("ls", shell=True)
+
+    def test_execute_script_with_static_argument(self):
+        with patch("subprocess.call") as call_mock, patch("sys.exit") as exit_mock:
+            helpers.execute_script("ls -l")
+            call_mock.assert_called_with("ls -l", shell=True)
+
+    def test_execute_script_with_dynamic_arguments_and_escape_if_necessary(self):
+        with patch("subprocess.call") as call_mock, patch("sys.exit") as exit_mock:
+            helpers.execute_script("ls", ["-l", "somefile; rm -rf ~"])
+            call_mock.assert_called_with("ls -l 'somefile; rm -rf ~'", shell=True)
+
+    def test_exit_if_config_not_present(self):
+        with patch("os.path.isfile", return_value=False) as isfile_mock, patch(
+            "sys.exit"
+        ) as exit_mock, patch("toml.load") as toml_mock:
+            helpers.read_config()
+            args, kwargs = exit_mock.call_args
+            assert args[0] == 1
+
+    def test_exit_if_config_malformed(self):
+        with patch("os.path.isfile", return_value=True) as isfile_mock, patch(
+            "sys.exit"
+        ) as exit_mock, patch(
+            "toml.load", side_effect=toml.TomlDecodeError("Error", "", 0)
+        ) as toml_mock:
+            helpers.read_config()
+            args, kwargs = exit_mock.call_args
+            assert args[0] == 1
